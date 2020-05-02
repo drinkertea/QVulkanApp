7a63adf5155a2ac3c3a86e9385b828ce013f4464
drinkertea <23703391+drinkertea@users.noreply.github.com>
2020-05-02 21:34:55 +0300

    shader

---
+++ source/renderer/private/Shader.cpp
@@ -0,0 +1,49 @@
+#include "Texture.h"
+#include "VulkanUtils.h"
+
+#include "ScopeCommandBuffer.h"
+#include "MappedData.h"
+
+#include <QVulkanFunctions>
+#include "Shader.h"
+
+namespace Vulkan
+{
+    VkShaderStageFlagBits GetVulkanStage(ShaderType type)
+    {
+        switch (type)
+        {
+        case ShaderType::vertex:   return VK_SHADER_STAGE_VERTEX_BIT;
+        case ShaderType::fragment: return VK_SHADER_STAGE_FRAGMENT_BIT;
+        default: throw std::logic_error("Invalid enum value");
+        }
+    }
+
+    Shader::Shader(const IDataProvider& data, ShaderType type, const QVulkanWindow& window)
+        : device(window.device())
+        , functions(*window.vulkanInstance()->deviceFunctions(window.device()))
+        , stage(GetVulkanStage(type))
+    {
+        VkShaderModuleCreateInfo shader_info{};
+        shader_info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        shader_info.codeSize = data.GetSize();
+        shader_info.pCode = reinterpret_cast<const uint32_t*>(data.GetData());
+
+        Vulkan::VkResultSuccess(functions.vkCreateShaderModule(device, &shader_info, nullptr, &shader_module));
+    }
+
+    Shader::~Shader()
+    {
+        functions.vkDestroyShaderModule(device, shader_module, nullptr);
+    }
+
+    VkShaderModule Shader::GetModule() const
+    {
+        return shader_module;
+    }
+
+    VkShaderStageFlagBits Shader::GetStage() const
+    {
+        return stage;
+    }
+}
---
+++ source/renderer/private/Shader.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QVulkanWindow>
+
+#include "IRenderer.h"
+
+namespace Vulkan
+{
+    class Shader
+        : public IShader
+    {
+    public:
+        Shader(const IDataProvider& data, ShaderType type, const QVulkanWindow& window);
+        ~Shader() override;
+
+        VkShaderModule GetModule() const;
+        VkShaderStageFlagBits GetStage() const;
+
+    private:
+        QVulkanDeviceFunctions& functions;
+        VkDevice device = nullptr;
+
+        VkShaderStageFlagBits stage = VK_SHADER_STAGE_VERTEX_BIT;
+        VkShaderModule shader_module = nullptr;
+    };
+
+}
