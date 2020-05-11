5543a43778a40e4dd3046b2dd3d09c4810c1a609
drinkertea <23703391+drinkertea@users.noreply.github.com>
2020-05-11 22:24:33 +0300

    plane shifting impl

--- source/scene/ChunkUtils.h
+++ source/scene/ChunkUtils.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <tuple>
 #include <cstdint>
+#include <vector>
 
 namespace Scene
 {
@@ -49,6 +50,73 @@ struct vec2
 using vec2i = vec2<int32_t>;
 
 uint32_t GetRank(const vec2i& mid, const vec2i& pos);
+
+template <typename T>
+void ShiftPlane(const vec2i& translation, std::vector<std::vector<T>>& plane)
+{
+    if (plane.empty())
+        return;
+
+    int n = static_cast<int>(plane.size());
+    int m = static_cast<int>(plane.front().size());
+    int x_from = 0;
+    int x_to = n;
+    if (translation.x > 0)
+    {
+        for (int i = 0; i < std::min(translation.x, n); ++i)
+        {
+            for (auto& x : plane[i])
+                x = {};
+        }
+        for (int i = 0; i + translation.x < n; ++i)
+        {
+            std::swap(plane[i], plane[i + translation.x]);
+        }
+        x_to = n - translation.x;
+
+    }
+    else if (translation.x < 0)
+    {
+        for (int i = n - 1; i >= -translation.x; --i)
+        {
+            std::swap(plane[i], plane[i + translation.x]);
+        }
+        x_from = std::min(-translation.x, n);
+        for (int i = 0; i < x_from; ++i)
+        {
+            for (auto& x : plane[i])
+                x = {};
+        }
+    }
+    if (translation.y < 0)
+    {
+        for (int k = x_from; k < x_to; ++k)
+        {
+            for (int i = m - 1; i >= -translation.y; --i)
+            {
+                std::swap(plane[k][i], plane[k][i + translation.y]);
+            }
+            for (int i = 0; i < std::min(-translation.y, m); ++i)
+            {
+                plane[k][i] = {};
+            }
+        }
+    }
+    else if (translation.y > 0)
+    {
+        for (int k = x_from; k < x_to; ++k)
+        {
+            for (int i = 0; i < std::min(translation.y, m); ++i)
+            {
+                plane[k][i] = {};
+            }
+            for (int i = 0; i + translation.y < m; ++i)
+            {
+                std::swap(plane[k][i], plane[k][i + translation.y]);
+            }
+        }
+    }
+}
 
 }
 
--- source/scene/tests/ChunkUtilsTests.cpp
+++ source/scene/tests/ChunkUtilsTests.cpp
@@ -72,3 +72,261 @@ TEST(ChunkUtilsTests, get_rank_test)
             );
 }
 
+TEST(ChunkUtilsTests, ShiftTest1)
+{
+    std::vector<std::vector<int>> before = {
+        { 1, 2 },
+        { 4, 5 },
+        { 7, 8 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 7, 8 },
+        { 0, 0 },
+        { 0, 0 },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(2, 0), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest2)
+{
+    std::vector<std::vector<int>> before = {
+        { 1, 2 },
+        { 1, 2 },
+        { 4, 5 },
+        { 7, 8 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 4, 5 },
+        { 7, 8 },
+        { 0, 0 },
+        { 0, 0 },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(2, 0), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest3)
+{
+    std::vector<std::vector<int>> before = {
+        { 1, 2 },
+        { 4, 5 },
+        { 7, 8 },
+        { 9, 9 }
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 0, 0 },
+        { 0, 0 },
+        { 1, 2 },
+        { 4, 5 },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(-2, 0), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest4)
+{
+    std::vector<std::vector<int>> before = {
+        { 1, 2 },
+        { 4, 5 },
+        { 7, 8 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 0, 0 },
+        { 0, 0 },
+        { 0, 0 },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(30, 0), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest5)
+{
+    std::vector<std::vector<int>> before = {
+        { 1, 2 },
+        { 4, 5 },
+        { 7, 8 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 0, 0 },
+        { 0, 0 },
+        { 0, 0 },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(-30, 0), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest6)
+{
+    std::vector<std::vector<int>> before = {
+        { 8, 1, 2, 3 },
+        { 8, 4, 5, 6 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 2, 3, 0, 0 },
+        { 5, 6, 0, 0 },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(0, 2), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest7)
+{
+    std::vector<std::vector<int>> before = {
+        { 1, 2, 3, 3 },
+        { 4, 5, 6, 3 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 0, 0, 1, 2, },
+        { 0, 0, 4, 5, },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(0, -2), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest8)
+{
+    std::vector<std::vector<int>> before = {
+        { 1, 2, 3, 3 },
+        { 4, 5, 6, 3 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 0, 0, 0, 0, },
+        { 0, 0, 0, 0, },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(0, -20), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest9)
+{
+    std::vector<std::vector<int>> before = {
+        { 1, 2, 3, 3 },
+        { 4, 5, 6, 3 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 0, 0, 0, 0, },
+        { 0, 0, 0, 0, },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(0, 20), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest10)
+{
+    std::vector<std::vector<int>> before = {
+        { 11, 12, 13 },
+        { 21, 22, 23 },
+        { 31, 32, 33 },
+        { 41, 42, 43 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 32, 33, 0 },
+        { 42, 43, 0 },
+        { 0,  0,  0 },
+        { 0,  0,  0 },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(2, 1), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest11)
+{
+    std::vector<std::vector<int>> before = {
+        { 11, 12, 13 },
+        { 21, 22, 23 },
+        { 31, 32, 33 },
+        { 41, 42, 43 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 0,  0,  0 },
+        { 0, 11, 12 },
+        { 0, 21, 22 },
+        { 0, 31, 32 },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(-1, -1), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest12)
+{
+    std::vector<std::vector<int>> before = {
+        { 11, 12, 13, 14, 15 },
+        { 21, 22, 23, 24, 25 },
+        { 31, 32, 33, 34, 35 },
+        { 41, 42, 43, 44, 45 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        {  0,  0, 0, 0, 0, },
+        { 14, 15, 0, 0, 0, },
+        { 24, 25, 0, 0, 0, },
+        { 34, 35, 0, 0, 0, },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(-1, 3), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest13)
+{
+    std::vector<std::vector<int>> before = {
+        { 11, 12, 13, 14, 15 },
+        { 21, 22, 23, 24, 25 },
+        { 31, 32, 33, 34, 35 },
+        { 41, 42, 43, 44, 45 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 0, 0, 0, 0, 41, },
+        { 0, 0, 0, 0,  0, },
+        { 0, 0, 0, 0,  0, },
+        { 0, 0, 0, 0,  0, },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(3, -4), before);
+    EXPECT_EQ(before, after);
+}
+
+TEST(ChunkUtilsTests, ShiftTest14)
+{
+    std::vector<std::vector<int>> before = {
+        { 11, 12, 13, 14, 15 },
+        { 21, 22, 23, 24, 25 },
+        { 31, 32, 33, 34, 35 },
+        { 41, 42, 43, 44, 45 },
+    };
+
+    std::vector<std::vector<int>> after = {
+        { 0, 0, 0, 0,  0, },
+        { 0, 0, 0, 0,  0, },
+        { 0, 0, 0, 0,  0, },
+        { 0, 0, 0, 0,  0, },
+    };
+
+    Scene::utils::ShiftPlane(vec2i(4, -5), before);
+    EXPECT_EQ(before, after);
+}
+
