6243a04c8686284011969644e66d5573b90b755f
Boris Galochkin <23703391+drinkertea@users.noreply.github.com>
2020-05-18 19:12:44 +0300

    Create get_qt.cmake

---
+++ cmake/get_qt.cmake
@@ -0,0 +1,60 @@
+macro(get_qt _version)
+  if (EXISTS ${PROJECT_BINARY_DIR}/Qt)
+    message(STATUS "Qt already downloaded at: ${PROJECT_BINARY_DIR}/Qt, to force download remove this directory.")
+    return()
+  endif()
+
+  string(REPLACE "." "" no_dot_ver ${_version})
+
+  if (WIN32)
+    set(qt_platform "windows_x86")
+    set(qt_compiler "win64_msvc2019_64")
+    set(qt_unpacked "msvc2019_64")
+  else()
+    set(qt_platform "linux_x64")
+    set(qt_compiler "gcc_64")
+  endif()
+  set(_url "http://download.qt.io/online/qtsdkrepository/${qt_platform}/desktop/qt5_${no_dot_ver}/qt.qt5.${no_dot_ver}.${qt_compiler}")
+
+  set(DOWNLOAD_TEMP_DIR "_downloaded")
+  set(file ${PROJECT_BINARY_DIR}/${DOWNLOAD_TEMP_DIR}/page.html)
+  message(STATUS "file: ${file}")
+
+  file(DOWNLOAD
+    ${_url}
+    ${file}
+    STATUS status
+    SHOW_PROGRESS
+  )
+
+  list(GET status 0 status_code)
+  list(GET status 1 status_string)
+
+  if(NOT status_code EQUAL 0)
+    message(FATAL_ERROR "error: downloading '${_url}' failed
+      status_code: ${status_code}
+      status_string: ${status_string}
+      log: ${log}
+    ")
+  endif()
+
+  file(STRINGS ${file} qt_links)
+  foreach(qt_link ${qt_links})
+    string(REGEX MATCH "\"${_version}.*qtbase.*7z\"" _ "${qt_link}")
+    set(fname ${CMAKE_MATCH_0})
+    if("${fname}" STREQUAL "")
+      continue()
+    endif()
+    message(STATUS "File found: ${fname}")
+    break()
+  endforeach()
+
+  string(REPLACE "\"" "" fname ${fname})
+
+  include(download_and_extract.cmake)
+  download_and_extract(${_url}/${fname} ${PROJECT_BINARY_DIR}/QtTemp)
+
+  file(RENAME ${PROJECT_BINARY_DIR}/QtTemp/${_version}/${qt_unpacked} ${PROJECT_BINARY_DIR}/Qt)
+
+  file(REMOVE_RECURSE ${PROJECT_BINARY_DIR}/QtTemp)
+endmacro()
